3f6573f1499b62a9d716b9a698dbd6a52372e3f2
Philippe Roemer <philipperoemer@yahoo.com>
2021-03-13 16:58:23 -0500

    App updated - Initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Tabletop from "tabletop";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        Tabletop.init({
+            key: "1wf_6Dok_yo5qJt6InaVF6UVisDTeh7J-BOi2dxOaBEo",
+            simpleSheet: true,
+        })
+            .then((data) => setData(data))
+            .catch((err) => console.warn(err));
+    }, []);
+
+    function InitialGeneratedRecipe() {
+        const Recipe = data;
+        const RandomRecipe = Recipe[Math.floor(Math.random() * Recipe.length)];
+        document.getElementById("RecipeName").innerHTML = RandomRecipe.Name;
+        document.getElementById("RecipeLink").innerHTML = "<a href='" + RandomRecipe.Link + "' target='_blank';>Recipe Link</a>";
+        document.getElementById("RecipeImage").innerHTML = "<img src='" + RandomRecipe.Image + "' class='recipeImage'/>";
+        document.getElementById("BigButton").style.display = "none";
+        document.getElementById("test").style.display = "flex";
+    }
+
+    function GenerateRecipe() {
+        const Recipe = data;
+        const RandomRecipe = Recipe[Math.floor(Math.random() * Recipe.length)];
+        document.getElementById("RecipeName").innerHTML = RandomRecipe.Name;
+        document.getElementById("RecipeLink").innerHTML = "<a href='" + RandomRecipe.Link + "' target='_blank';>Recipe Link</a>";
+        document.getElementById("RecipeImage").innerHTML = "<img src='" + RandomRecipe.Image + "' class='recipeImage'/>";
+    }
+
+    window.onload = function () {
+        console.log("testing");
+        document.getElementById("test").style.display = "none";
+    };
+
+    return (
+        <div className="container">
+            <h1 className="title">Random Recipe Generator</h1>
+            <div onClick={InitialGeneratedRecipe} className="buttonBig" id="BigButton">
+                <h2>Random Recipe</h2>
+            </div>
+            <h3 id="RecipeName"></h3>
+            <p id="RecipeLink"></p>
+            <div id="RecipeImage"></div>
+            <div id="test" onClick={GenerateRecipe} className="buttonSmall">
+                <h3>Next</h3>
+            </div>
+        </div>
+    );
+}
 export default App;
